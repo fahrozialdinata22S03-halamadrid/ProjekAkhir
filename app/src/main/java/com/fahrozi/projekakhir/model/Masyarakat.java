76478b9d7007c30c1c6923ba6691fca855bfc157
fahrozialdinata22S03-halamadrid <fahrozialdinata22s03@gmail.com>
2025-11-07 08:29:55 +0700

    Implementasi Part 1: Membuat model Masyarakat dan DbHelper

---
+++ app/src/main/java/com/fahrozi/projekakhir/model/Masyarakat.java
@@ -0,0 +1,32 @@
+package com.fahrozi.projekakhir.model;
+
+import java.io.Serializable;
+
+public class Masyarakat implements Serializable {
+    private int id;
+    private String nik, nama;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNIK() {
+        return nik;
+    }
+
+    public void setNIK(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
